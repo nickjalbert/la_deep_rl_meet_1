3688e40b50483d2e800f3becf9c6c2c5cada4ee6
Nick Jalbert <nickjalbert@gmail.com>
2020-03-09 14:55:28 -0700

    Minor comment updates for env

--- environment.py
+++ environment.py
@@ -6,6 +6,7 @@ import gym
 class CorridorEnv(gym.Env):
     metadata = {"render.modes": ["human"]}
 
+    # Check [env_config] for corridor length, default to 10
     def __init__(self, env_config=None):
         self.env_config = env_config if env_config else {}
         self.length = int(self.env_config.get("length", 10))
@@ -14,12 +15,13 @@ class CorridorEnv(gym.Env):
         self.reset()
 
     def step(self, action):
-        #  make clear position 0
         # =========
-        # TODO - Implement a model of a 1D corridor:
-        #   * action is either a 0 (move left) or 1 (move right)
+        # TODO - Implement a 1D corridor environment:
         #   * self.position is the agent's current position in the env
-        #   * the agent hits wall if it walks left at self.position = 0
+        #   * agent starts in self.position 0 (far left)
+        #   * agent must walk right to the end of the corridor (self.length)
+        #   * action is either a 0 (move left) or 1 (move right)
+        #   * the agent hits wall if it walks left at self.position == 0
         #   * the agent succeeds when self.position == self.length
         #   * reward scheme motivates the agent to traverse the corridor
         # =========
--- solutions/environment.py
+++ solutions/environment.py
@@ -6,6 +6,7 @@ import gym
 class CorridorEnv(gym.Env):
     metadata = {"render.modes": ["human"]}
 
+    # Check [env_config] for corridor length, default to 10
     def __init__(self, env_config=None):
         self.env_config = env_config if env_config else {}
         self.length = int(self.env_config.get("length", 10))
@@ -14,10 +15,13 @@ class CorridorEnv(gym.Env):
         self.reset()
 
     def step(self, action):
+        # If done, we stand still
         if self.done:
             return (self.position, 0, self.done, {})
+        # Walk left if we are not at the far left of the corridor
         if action == 0 and self.position > 0:
             self.position -= 1
+        # Walk right if we are not at the far right of the corridor
         if action == 1 and self.position < self.length:
             self.position += 1
         return (self.position, -1, self.done, {})
